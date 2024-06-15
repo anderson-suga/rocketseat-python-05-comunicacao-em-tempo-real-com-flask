b2619aaa5bac55a0e92d857f0f3cdb84c50d74ac
Anderson Suga <anderson.suga@gmail.com>
2024-06-14 21:19:12 -0300

    Armazenando QR Code e criando rota de exibição de imagens

--- payments/pix.py
+++ payments/pix.py
@@ -7,7 +7,7 @@ class Pix:
         pass
 
     def create_payment(self):
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
 
         bash_payment = f"hash_payment_{bank_payment_id}"
 
