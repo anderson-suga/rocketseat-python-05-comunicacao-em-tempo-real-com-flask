0d4bfc37e66d209d9391ce513f730a296c368749
Anderson Suga <anderson.suga@gmail.com>
2024-06-14 21:33:36 -0300

    Adicionando Dados Dinâmicos na Página de Pagamentos

--- payments/pix.py
+++ payments/pix.py
@@ -17,5 +17,5 @@ class Pix:
 
         return {
             "bank_payment_id": bank_payment_id,
-            "qr_code_path": f"qr_code_payment_{bank_payment_id}.png",
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}",
         }
