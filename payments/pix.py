00d3688cc3a8635b007daeffcdae8f575554fd38
Anderson Suga <anderson.suga@gmail.com>
2024-06-14 21:05:30 -0300

    Criando o Pagamento Pix com QR Code

---
+++ payments/pix.py
@@ -0,0 +1,21 @@
+import uuid
+import qrcode
+
+
+class Pix:
+    def __init__(self) -> None:
+        pass
+
+    def create_payment(self):
+        bank_payment_id = uuid.uuid4()
+
+        bash_payment = f"hash_payment_{bank_payment_id}"
+
+        img = qrcode.make(bash_payment)
+
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+        return {
+            "bank_payment_id": bank_payment_id,
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}.png",
+        }
